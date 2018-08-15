5b23b2e9c6f9ecd3635b596d0217e2be5748fc7f
patkahnke <pat.kahnke@banno.com>
2018-08-15 14:36:40 -0500

    adds StorePicker component

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-// let's go!
-alert('Working');
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker';
+
+render(<StorePicker />, document.querySelector('#main'));
