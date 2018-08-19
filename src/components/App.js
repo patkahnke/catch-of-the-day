df6800c5274f0da3043f286e787e6bdc8b8ecb34
patkahnke <pat.kahnke@banno.com>
2018-08-18 21:11:41 -0500

    adds addToOrder function

--- src/components/App.js
+++ src/components/App.js
@@ -17,6 +17,12 @@ class App extends React.Component {
     this.setState({ fishes });
   };
 
+  addToOrder = key => {
+    const order = { ...this.state.order };
+    order[key] = order[key] + 1 || 1;
+    this.setState({ order });
+  }
+
   loadSampleFishes = () => {
     this.setState({ fishes: sampleFishes});
   }
@@ -27,14 +33,20 @@ class App extends React.Component {
         <div className="menu">
           <Header tagline="Fresh Seafood Market" />
           <ul className="fishes">
-          {Object.keys(this.state.fishes).map(key => <Fish key={key} details={this.state.fishes[key]}/>)}
+          {Object.keys(this.state.fishes).map(key => (
+            <Fish
+              key={key}
+              index={key}
+              details={this.state.fishes[key]}
+              addToOrder={this.addToOrder}
+              />))}
           </ul>
         </div>
         <Order />
         <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
-  }
+  };
 }
 
 export default App;
--- src/components/Fish.js
+++ src/components/Fish.js
@@ -3,7 +3,8 @@ import { formatPrice } from "../helpers";
 
 class Fish extends React.Component {
   render() {
-    const { image, name, price, desc, status} = this.props.details;
+    const { image, name, price, desc, status} = this.props.details
+    const isAvailable = status === "available";
     return (
       <li className="menu-fish">
         <img src={image} alt={name} />
@@ -12,11 +13,12 @@ class Fish extends React.Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button>Add To Cart</button>
+        <button disabled={!isAvailable} onClick={() => this.props.addToOrder(this.props.index)}>
+          {isAvailable ? "Add To Order" : "Sold Out!"}
+        </button>
       </li>
-
-    )
-  }
+    );
+  };
 }
 
 export default Fish;
